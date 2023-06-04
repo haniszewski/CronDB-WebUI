3f1959c3077f02dbe40ee931afd5ec3e23a983fe
SarokaAnton <tonysorokaol@gmail.com>
2023-06-04 22:48:22 +0200

    Changed background in Login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoutes from './routes/PrivateRoutes';
 import Login from './components/Login/Login';
-import Databases from './components/Databases/Databases'; 
+import Databases from './containers/Databases/Databases'; 
 
 const App = () => {
   return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav>
+        <ul>
+            <li>
+                <Link to="/databases">Databases</Link>
+            </li>
+            <li>
+                <Link to="/metrics">Metrics</Link>
+            </li>
+            <li>
+                <Link to="/backups">Backups</Link>
+            </li>
+            <li>
+                <Link to="/schedules">Schedules</Link>
+            </li>
+            <li>
+                <Link to="/panel">Panel</Link>
+            </li>
+            
+        </ul>
+        </nav>
+  );
+};
 
 export default Navbar
