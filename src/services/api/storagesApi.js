8bed5a363daf66505e9efa51af401c29b7cc52f5
SarokaAnton <tonysorokaol@gmail.com>
2023-06-14 13:27:57 +0200

    Added fetches to Storages

---
+++ src/services/api/storagesApi.js
@@ -0,0 +1,19 @@
+export const getAllStorages = async () => {
+    try {
+        const response = await fetch(process.env.REACT_APP_SERVER_URL + '/storages/ftp', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + getToken()
+            },
+        });
+        if (response.ok) {
+          const storages = await response.json();
+          return storages;
+        } else {
+          console.log('Failed to retrieve storages');
+        }
+      } catch (error) {
+        console.log(`Error: ${error}`);
+      }
+}
---
+++ src/services/storages/storagesService.js
@@ -0,0 +1,76 @@
+export const testConnectionToStorage = async (host, port, login, pass, passive) => {
+    try {
+        const response = await fetch(process.env.REACT_APP_SERVER_URL + '/storages/ftp/test', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + getToken()
+            },
+            body: JSON.stringify({
+                ftpHost: host,
+                ftpPort: port,
+                ftpLogin: login,
+                ftpPass: pass,
+                passive: passive
+            })
+        });
+        if (response.ok) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error) {
+        console.log(`Error: ${error}`);
+        return false;
+      }
+}
+
+export const addStorage = async (host, port, login, pass, passive) => {
+    try {
+        const response = await fetch(process.env.REACT_APP_SERVER_URL + '/storages/ftp/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + getToken()
+            },
+            body: JSON.stringify({
+                ftpHost: host,
+                ftpPort: port,
+                ftpLogin: login,
+                ftpPass: pass,
+                passive: passive
+            })
+        });
+        if (response.ok) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error) {
+        console.log(`Error: ${error}`);
+        return false;
+      }
+}
+
+export const deleteStorage = async (storageId) => {
+    try {
+        const response = await fetch(process.env.REACT_APP_SERVER_URL + `/storages/ftp/${id}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        }
+        });
+    
+        if (response.ok) {
+          console.log('Storage deleted successfully');
+          return true;
+        } else {
+          console.log('Failed to delete storage');
+          return false;
+        }
+      } catch (error) {
+        console.log(`Error: ${error}`);
+        return false;
+      }
+}
